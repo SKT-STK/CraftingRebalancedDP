59e2cfe0acc3cd2d304311d1588619b1a12cab58
SKT-STK <kwasny.sebastian13@gmail.com>
2024-12-16 01:58:37 +0100

    made it so that now you craft 8 stairs instead of 4 (6+0.25*6 = ~8 (7.5))

---
+++ stairs_generator.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+DIR_PATH = r'D:\Games\Minecraft\ModrinthApp\profiles\NIGGER\.fabric\remappedJars\minecraft-1.21.3-0.16.9\client-intermediary\data\minecraft\recipe'
+OUT_PATH = r'D:\Games\Minecraft\ModrinthApp\profiles\NIGGER\saves\crafting_rebalanced_dp\datapacks\CraftingRebalancedDP\data\minecraft\recipe'
+
+stairs = []
+
+for filename in os.listdir(DIR_PATH):
+  if '_stairs' in filename and '_from' not in filename:
+    with open(os.path.join(DIR_PATH, filename)) as file:
+      stairs.append(dict(filename = filename, recipe = json.load(file)))
+
+for stair in stairs:
+  stair['recipe']['result']['count'] = 8
+  
+  with open(os.path.join(OUT_PATH, stair['filename']), 'w') as file:
+    json.dump(stair['recipe'], file, indent = 4)
