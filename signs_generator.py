cc6539d7c76bed7420323001a8a4d507a2795cb2
SKT-STK <kwasny.sebastian13@gmail.com>
2024-12-16 23:01:41 +0100

    fixed signs not being stackable up to 64 after being placed and destroyed

--- signs_generator.py
+++ signs_generator.py
@@ -1,14 +1,16 @@
 import json
 import os
 
-DIR_PATH = r'D:\Games\Minecraft\ModrinthApp\profiles\NIGGER\.fabric\remappedJars\minecraft-1.21.3-0.16.9\client-intermediary\data\minecraft\recipe'
-OUT_PATH = r'D:\Games\Minecraft\ModrinthApp\profiles\NIGGER\saves\crafting_rebalanced_dp\datapacks\CraftingRebalancedDP\data\minecraft\recipe'
+CRAFTING_DIR_PATH = r'D:\Games\Minecraft\ModrinthApp\profiles\NIGGER\.fabric\remappedJars\minecraft-1.21.3-0.16.9\client-intermediary\data\minecraft\recipe'
+CRAFTING_OUT_PATH = r'D:\Games\Minecraft\ModrinthApp\profiles\NIGGER\saves\crafting_rebalanced_dp\datapacks\CraftingRebalancedDP\data\minecraft\recipe'
+LOOT_TABLE_DIR_PATH = r'D:\Games\Minecraft\ModrinthApp\profiles\NIGGER\.fabric\remappedJars\minecraft-1.21.3-0.16.9\client-intermediary\data\minecraft\loot_table\blocks'
+LOOT_TABLE_OUT_PATH = r'D:\Games\Minecraft\ModrinthApp\profiles\NIGGER\saves\crafting_rebalanced_dp\datapacks\CraftingRebalancedDP\data\minecraft\loot_table\blocks'
 
 signs = []
 
-for filename in os.listdir(DIR_PATH):
+for filename in os.listdir(CRAFTING_DIR_PATH):
   if '_sign' in filename:
-    with open(os.path.join(DIR_PATH, filename)) as file:
+    with open(os.path.join(CRAFTING_DIR_PATH, filename)) as file:
       signs.append(dict(filename = filename, recipe = json.load(file)))
 
 for h_sign in [s for s in signs if '_hanging_' in s['filename']]:
@@ -23,5 +25,24 @@ for sign in signs:
     sign['recipe']['result']['components'] = {}
   sign['recipe']['result']['components']['minecraft:max_stack_size'] = 64
   
-  with open(os.path.join(OUT_PATH, sign['filename']), 'w') as file:
+  with open(os.path.join(CRAFTING_OUT_PATH, sign['filename']), 'w') as file:
     json.dump(sign['recipe'], file, indent = 4)
+
+signs = []
+
+for filename in os.listdir(LOOT_TABLE_DIR_PATH):
+  if '_sign' in filename:
+    with open(os.path.join(LOOT_TABLE_DIR_PATH, filename)) as file:
+      signs.append(dict(filename = filename, loot_table = json.load(file)))
+      
+for sign in signs:
+  print(sign['filename'])
+  if 'functions' not in sign['loot_table']:
+    sign['loot_table']['functions'] = [dict(function = 'minecraft:set_components', components = {})]
+  sign['loot_table']['functions'][0]['components']['minecraft:max_stack_size'] = 64
+  
+  if 'pools' not in sign['loot_table']:
+    sign['loot_table']['pools'] = [dict(bonus_rolls = 0.0, rolls = 1.0, conditions = [dict(condition = 'minecraft:survives_explosion')], entries = [dict(type = 'minecraft:item', name = f'minecraft:{sign['filename'].replace('.json', '')}')])]
+  
+  with open(os.path.join(LOOT_TABLE_OUT_PATH, sign['filename']), 'w') as file:
+    json.dump(sign['loot_table'], file, indent = 4)
